77444ab324d4288d3f20cb7ee9906d47313397dc
Torsten Gellert <torsten.gellert@zalando.de>
2016-10-14 19:28:02 +0200

    intersection tests for triangles fixed

--- src/test/scala/TriangleSpec.scala
+++ src/test/scala/TriangleSpec.scala
@@ -6,35 +6,48 @@ import org.specs2.{ScalaCheck, Specification}
 
 class TriangleSpec  extends Specification with ScalaCheck { def is = s2"""
    A triangle should
-      be intersected frontally ${intersectFrontal}
-      be intersected frontally correctly {hittingTest.verbose}
-      be intersected frontally with correct dist {hittingTestDist.verbose}
-      be missed frontally with correct dist {hittingTestDist.verbose}
+      be intersected from both sides $intersectFrontal
+      be intersected accordingly to distance $intersectAccordingToDistance
+      be intersected frontally correctly ${hittingTest.verbose}
+      be intersected frontally with large enough distance {hittingTestDist.verbose}
+      be missed frontally due to short distance {hittingTestDist.verbose}
   """
-
+//TODO: fix the three broken tests
   def a = Vector3(-1,-1, 1)
   def b = Vector3( 1,-1, 1)
-  def c = Vector3(-1, 1, 1)
+  def c = Vector3( 1, 1, 1)
 
   def triangle = Triangle(a,b,c)
 
   def intersectFrontal = {
 
-    val hit = triangle.intersect( Ray( Vector3(-0.2f,-0.2f,0.0f), Vector3.Z))
-    hit shouldNotEqual None
+    val hitFront = triangle.intersect( Ray( Vector3(-0.2f,-0.2f,0.0f),  Vector3.Z))
+    val hitBack =  triangle.intersect( Ray( Vector3(-0.2f,-0.2f,2.0f), -Vector3.Z))
+
+    (hitFront shouldNotEqual None) and (hitBack shouldNotEqual None)
   }
 
-  val hittingTest      = forAll { (r: Ray) =>  triangle.intersect(r) != None }
-  val hittingTestDist  = forAll { (r: Ray) =>  triangle.intersect(r, 1.1f) }
-  val missingTestDist  = forAll { (r: Ray) =>  !triangle.intersect(r, .9f) }
+  def intersectAccordingToDistance = {
+    val hitFront  = triangle.intersect( Ray( Vector3(-0.2f,-0.2f,0.0f), Vector3.Z) , 1+.1f)
+    val missFront = triangle.intersect( Ray( Vector3(-0.2f,-0.2f,0.0f), -Vector3.Z), 1-.1f)
+
+    val hitBack =  triangle.intersect( Ray( Vector3(-0.2f,-0.2f,2.0f), -Vector3.Z),  1+.1f)
+    val missBack =  triangle.intersect( Ray( Vector3(-0.2f,-0.2f,2.0f), -Vector3.Z), 1-.1f)
+
+
+    (hitFront, hitBack, missFront, missBack)  should beEqualTo((true,true,false,false))
+  }
+
+  val hittingTest      = forAll { (r: Ray) =>  triangle.intersect(r) != None } //FAILS
+  val hittingTestDist  = forAll { (r: Ray) =>  triangle.intersect(r, 1.1f) }   //FAILS
+  val missingTestDist  = forAll { (r: Ray) =>  !triangle.intersect(r,  .9f)}   //FAILS
 
   implicit lazy val HittingRayGen: Arbitrary[Ray] =
     Arbitrary {
       for{ x : Float <- Gen.choose(-.9f,.9f)
-           y : Float <- Gen.choose(-.9f, .9f-x)
+           y : Float <- Gen.choose(-.9f, x )
       } yield Ray(Vector3(x,y,0) , Vector3.Z)
     }
-
 
 
 
