85ee0d0763458565caea458d0573f8c0b0df38da
Torsten Gellert <t.gellert@gmail.com>
2016-11-03 23:01:17 +0100

    Checkerboardmaterial and JSon parsing added

--- src/main/scala/Material.scala
+++ src/main/scala/Material.scala
@@ -18,11 +18,13 @@ object Material {
   def apply(`type`: String, data: JsValue): Material = {
     (`type` match {
       case "SingleColorMaterial" => Json.fromJson[SingleColorMaterial](data)(singleColorMaterialFmt)
+      case "CheckerMaterial" => Json.fromJson[CheckerMaterial](data)(checkerMaterialFmt)
     }).get
   }
 
   implicit val materialFmt : Format[Material] = Json.format[Material]
   implicit val singleColorMaterialFmt : Format[SingleColorMaterial] = Json.format[SingleColorMaterial]
+  implicit val checkerMaterialFmt : Format[CheckerMaterial] = Json.format[CheckerMaterial]
 }
 
 final case class UnshadedColor(color: RGB, ambient: Float, diffuse: Float, spec: Float, reflective: Float, refractive: Float, n: Float, shininess: Float)
@@ -37,4 +39,14 @@ final case class SingleColorMaterial(name: String, c: RGB, ambient: Float, diffu
   require( Math.abs(ambient+diffuse+spec+reflective+refractive - 1) <= Material.EPS )
 
   override def getMat(position: Vector3) = UnshadedColor(c, ambient, diffuse, spec, reflective, refractive, n, shininess)
+}
+
+final case class CheckerMaterial(name: String, c1: RGB, c2: RGB, steps: Float, ambient: Float, diffuse: Float, spec: Float, reflective : Float = 0.05f,  refractive: Float = 0, n: Float = 1.33f, shininess: Float = 64) extends Material {
+  require( Math.abs(ambient+diffuse+spec+reflective+refractive - 1) <= Material.EPS )
+
+  private def mod(x : Float, m: Float) = (x%m + m)%m
+
+  private def inStep(pos: Float) : Boolean = mod(pos,2*steps) >= steps
+  private def inStep(pos: Vector3) : Boolean = inStep(pos.x) ^ inStep(pos.y) ^ inStep(pos.z)
+  override def getMat(position: Vector3) = UnshadedColor( if(inStep(position)) c1 else c2, ambient, diffuse, spec, reflective, refractive, n, shininess)
 }
