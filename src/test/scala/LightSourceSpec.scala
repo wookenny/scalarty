ceb84e951c833ca776c81bdb28a5c6f0f6752155
Torsten Gellert <t.gellert@gmail.com>
2017-06-30 20:32:28 +0200

    LightSource Tests

--- src/test/scala/LightSourceSpec.scala
+++ src/test/scala/LightSourceSpec.scala
@@ -1,8 +1,11 @@
 import color.RGB
 import lightning.{LightSource, PlaneLight, PointLight}
 import math.Vector3
+import org.specs2.matcher.MatchResult
 import org.specs2.{ScalaCheck, Specification}
 import play.api.libs.json.Json
+
+import scala.collection.immutable
 
 class LightSourceSpec extends Specification with ScalaCheck {
 
@@ -25,26 +28,48 @@ class LightSourceSpec extends Specification with ScalaCheck {
                               width = 0.2,
                               length= 0.4,
                               RGB.BLUE,
-                              power = 0.123)
+                              power = 124)
 
   val pointLight = PointLight(position = Vector3.ZERO,
                               RGB.RED,
                               power = 12)
 
   val intensityTestPointlight = {
-    todo
+    val power1: Double = planeLight.intensity(Vector3.Z*2)
+    val power2: Double = planeLight.intensity(Vector3.X, Some(Vector3.X*2))
+
+    (power1 should be equalTo 124/4) and
+      (power2 should be equalTo 124)
+
   }
 
   val intensityTestPlanelight = {
-    todo
+    val power: Double = pointLight.intensity(Vector3.Z*2)
+    val powerUnitDistance: Double = pointLight.intensity(Vector3.X)
+
+    (power should be equalTo 3) and
+      (powerUnitDistance should be equalTo 12)
   }
 
   val sampleTestPointlight = {
-    todo
+    val sample1 = planeLight.sample(n = 1).distinct
+    val sample2 = planeLight.sample(n = 2).distinct
+    val sample3 = planeLight.sample(n = 3).distinct
+
+    val correctSizes = sample3.size == 9 && sample2.size == 4 && sample1.size == 1
+    val positionOnLight = (sample1++sample2++sample3).map(p =>
+          p.x >= planeLight.position.x - planeLight.width
+      &&  p.x <= planeLight.position.x + planeLight.width
+      &&  p.z >= planeLight.position.z - planeLight.length
+      &&  p.z <= planeLight.position.z + planeLight.length
+      &&  Math.abs(p.y - planeLight.position.y)<0.001
+    ).distinct
+    (correctSizes should be equalTo true) and
+      (positionOnLight should contain(exactly(true)))
   }
 
   val sampleTestPlanelight = {
-    todo
+    pointLight.sample(n = 4) should contain(exactly(Vector3.ZERO))
   }
 
   val parsePointlight = parseLight(pointLight)
